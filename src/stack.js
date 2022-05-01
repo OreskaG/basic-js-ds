f27e47b8092a3cad0e99b79537cede232a8b0bcf
OreskaG <Ctepanrfo@gmail.com>
2022-05-01 14:32:59 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.Stack = []
+  }
+
+  push(element) {
+    this.Stack.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Stack.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let topElement = this.Stack.length - 1 ;
+    return this.Stack[topElement] ;
   }
 }
 
